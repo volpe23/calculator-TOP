6189cd9a9c58949d180c4184d7b9c6967d81c0a5
volpe23 <arozmianecs@gmail.com>
2022-12-27 10:23:17 +0200

    Added keyboard number input

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 const numbers = Array(10).fill().map((_, i) => (i)).reverse();
 numbers.push('.')
 numbers.splice(numbers.length-2, 0, '+/-');
+
+const input = document.querySelector('#inputField'); // Input field where numbers are added
+
 // Get the div where numbers will be 
 const numberDiv = document.querySelector('.numbers');
 
@@ -11,5 +14,27 @@ numbers.forEach(num => {
     if (num === 0) button.classList.add('zero')
     button.textContent = num;
     button.value = num;
+    if (typeof(num) === 'number') {
+        button.addEventListener('click', numberEvent);
+    } else {
+        console.log(typeof(num))
+    }
     numberDiv.appendChild(button);
+
 });
+
+function numberEvent(e) {
+    input.textContent += e.target.value
+}
+
+function operationEvent(e) {
+    console.log('hey')
+}
+
+window.addEventListener('keydown', e => {
+
+    const val = +(e.key);
+    if (val) {
+        input.textContent += val;
+    }
+})
